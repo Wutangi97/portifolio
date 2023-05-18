d21b29c28ad83b00ba33f1840153977f3fee7553
sam <sam@gmail.com>
2023-05-18 15:06:11 +0300

    modified

---
+++ index.js
@@ -0,0 +1,8 @@
+    
+let hello = document.querySelector("h1")
+let button = document.querySelector("button")
+
+button.addEventListener("click", function(){
+      
+    hello.classList.add("heading")
+})
